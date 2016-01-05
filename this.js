29c2eacb349d4910146b2377d305b506e61ac179
Elyager <elyager@gmail.com>
2016-01-05 14:55:56 -0700

    fix issue calling a method instead of the property directly

--- this.js
+++ this.js
@@ -1,19 +1,20 @@
 
 console.log(this) //contexto global
-console.log(this === window); //true
+console.log(this === window) //true
 
 var developer = {
   name : 'Erik',
   lastName : 'Ochoa',
   isAdult : true,
-  completeName: this.name + this.lastName
-
-};
+  completeName: function(){
+    return this.name + this.lastName
+  }
+}
  
 console.log(developer.name) // Erik
 console.log(developer.lastName) // Ochoa
 console.log(developer.isAdult) // true
-console.log(developer.completeName) // ErikOchoa
-
+//We call a function that is stored as a property of an object becoming a method
+console.log(developer.completeName()) // ErikOchoa
 
 
