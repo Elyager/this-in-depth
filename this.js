8687b1603f84c562db74f2cae2d5cacc8a577dd5
Elyager <elyager@gmail.com>
2016-01-05 18:57:39 -0700

    'Use of call and apply functions'

--- this.js
+++ this.js
@@ -6,14 +6,30 @@ var developer = {
   name : 'Erik',
   lastName : 'Ochoa',
   isAdult : true,
-  get completeName() {
-    return this.name + this.lastName
-  }
 }
- 
+var developerJr = {
+  name : 'Iker',
+  lastName : 'Ochoa',
+  isAdult : false,
+}
+
+var printName = function(){
+  console.log( this.name + ' ' + this.lastName );
+}
+
+var printNameAndAge = function(age){
+  console.log( this.name + ' ' + this.lastName + 'Age: ' + age);
+}
+
 console.log(developer.name) // Erik
 console.log(developer.lastName) // Ochoa
 console.log(developer.isAdult) // true
-console.log(developer.completeName) // ErikOchoa
+printName.call(developer)
+printNameAndAge.call(developer, 29)
+// .apply hace lo mismo sólo que permite mandar un arreglo como parametro
 
-
+console.log(developerJr.name) // Erik
+console.log(developerJr.lastName) // Ochoa
+console.log(developerJr.isAdult) // true
+printName.call(developerJr)
+printNameAndAge.call(developerJr, 13)
